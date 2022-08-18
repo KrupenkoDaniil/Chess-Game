b2abb5357b3b21cae52242ac1861705ebbcfe483
Mona Lisa <daniilkru24@gmail.com>
2022-08-19 09:59:27 +1000

    rooks_completed

--- pawn.js
+++ pawn.js
@@ -1,14 +1,16 @@
-import { board } from "./chess.js";
+import { board } from "./main.js";
 
 let enemyAhead = false;
 export const pawnCheck = (color, pos_x, pos_y) => {
+    const checkCellsAhead = document.querySelectorAll(`[data-x='${pos_x}']`);
+    const checkCellsOnLeft = document.querySelectorAll(`[data-x='${pos_x - 1}']`);
+    const checkCellsOnRight = document.querySelectorAll(`[data-x='${Number(pos_x) + 1}']`);
     if (color == 'w') {
 
         // Null enemies statuses
         enemyAhead = false;
 
         // find cells ahead
-        const checkCellsAhead = document.querySelectorAll(`[data-x='${pos_x}']`);
         for (let c = 0; c < checkCellsAhead.length; c++) {
             if ((checkCellsAhead[c].dataset.y == Number(pos_y) - 1) // if it is the right  coords
                 && (checkCellsAhead[c].classList.contains('game-board__cell')) // if it is the cell
@@ -32,7 +34,6 @@ export const pawnCheck = (color, pos_x, pos_y) => {
         }
 
         // find cells left
-        const checkCellsOnLeft = document.querySelectorAll(`[data-x='${pos_x - 1}']`);
         if (checkCellsOnLeft.length !== 0) {
             for (let c = 0; c < checkCellsOnLeft.length; c++) {
                 if (
@@ -40,21 +41,20 @@ export const pawnCheck = (color, pos_x, pos_y) => {
                     && (checkCellsOnLeft[c].classList.contains('game-board__cell'))
                     && (board[Number(pos_y) - 2][pos_x - 2] !== null)
                     && (board[Number(pos_y) - 2][pos_x - 2][0] !== 'w') // Checking if it is ally 
-                ) { 
+                ) {
                     checkCellsOnLeft[c].classList.add('game-board__cell_active');
                 } if (
                     (checkCellsOnLeft[c].dataset.y == Number(pos_y) - 1)
                     && (checkCellsOnLeft[c].classList.contains('figure'))
                     && (board[Number(pos_y) - 2][pos_x - 2] !== null)
                     && (board[Number(pos_y) - 2][pos_x - 2][0] !== 'w') // Checking if it is ally 
-                ) { 
+                ) {
                     checkCellsOnLeft[c].classList.add('figure_active');
                 }
             }
         }
 
         // find cells right
-        const checkCellsOnRight = document.querySelectorAll(`[data-x='${Number(pos_x) + 1}']`);
         if (checkCellsOnRight.length !== 0) {
             for (let c = 0; c < checkCellsOnRight.length; c++) {
                 if (
@@ -62,14 +62,14 @@ export const pawnCheck = (color, pos_x, pos_y) => {
                     && (checkCellsOnRight[c].classList.contains('game-board__cell'))
                     && (board[Number(pos_y) - 2][Number(pos_x)] !== null)
                     && (board[Number(pos_y) - 2][Number(pos_x)][0] !== 'w') // Checking if it is ally 
-                    ) { 
+                ) {
                     checkCellsOnRight[c].classList.add('game-board__cell_active');
                 } if (
                     (checkCellsOnRight[c].dataset.y == Number(pos_y) - 1)
                     && (checkCellsOnRight[c].classList.contains('figure'))
                     && (board[Number(pos_y) - 2][Number(pos_x)] !== null)
                     && (board[Number(pos_y) - 2][Number(pos_x)][0] !== 'w') // Checking if it is ally 
-                    ) { 
+                ) {
                     checkCellsOnRight[c].classList.add('figure_active');
                 }
             }
@@ -81,7 +81,6 @@ export const pawnCheck = (color, pos_x, pos_y) => {
         enemyAhead = false;
 
         // find cells ahead
-        const checkCellsAhead = document.querySelectorAll(`[data-x='${pos_x}']`);
         for (let c = 0; c < checkCellsAhead.length; c++) {
             if ((checkCellsAhead[c].dataset.y == Number(pos_y) + 1) // if it is the right  coords
                 && (checkCellsAhead[c].classList.contains('game-board__cell')) // if it is the cell
@@ -105,7 +104,6 @@ export const pawnCheck = (color, pos_x, pos_y) => {
         }
 
         // find cells left
-        const checkCellsOnLeft = document.querySelectorAll(`[data-x='${pos_x - 1}']`);
         if (checkCellsOnLeft.length !== 0) {
             for (let c = 0; c < checkCellsOnLeft.length; c++) {
                 if ((checkCellsOnLeft[c].dataset.y == Number(pos_y) + 1)
@@ -123,7 +121,6 @@ export const pawnCheck = (color, pos_x, pos_y) => {
         }
 
         // find cells right
-        const checkCellsOnRight = document.querySelectorAll(`[data-x='${Number(pos_x) + 1}']`);
         if (checkCellsOnRight.length !== 0) {
             for (let c = 0; c < checkCellsOnRight.length; c++) {
                 if ((checkCellsOnRight[c].dataset.y == Number(pos_y) + 1)
