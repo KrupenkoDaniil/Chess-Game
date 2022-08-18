ed34ad4145eef9b5a7e4e8e8de7faed9905e6c0d
Mona Lisa <daniilkru24@gmail.com>
2022-08-18 17:06:26 +1000

    separation_p1

---
+++ rook.js
@@ -0,0 +1,12 @@
+import { board } from "./chess.js";
+
+export const rookCheck = (color, pos_x, pos_y) => {
+    if (color === 'w') {
+
+        
+        let move1, move2, move3, move4;
+
+    } else {
+        console.log('Black');
+    }
+}
