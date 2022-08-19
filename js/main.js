9c8ad66f8771d41086a319ebd7434ba6463285dc
KrupenkoDaniil <108655963+KrupenkoDaniil@users.noreply.github.com>
2022-08-19 14:07:49 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,277 @@
+import { pawnCheck } from "./pawn.js";
+import { rookCheck } from "./rook.js";
+import { bishopCheck } from "./bishop.js";
+import { kingCheck } from "./king.js";
+
+const src = 'img/';
+let playerMove = true;
+
+// Game Board
+const gameBoard = document.querySelector('.game-board');
+const BP = 'bp';
+const BR = 'br';
+const BKN = 'bkn';
+const BB = 'bb';
+const BQ = 'bq';
+const BKI = 'bki';
+
+const WP = 'wp';
+const WR = 'wr';
+const WKN = 'wkn';
+const WB = 'wb';
+const WQ = 'wq';
+const WKI = 'wki';
+
+export let board = [
+    [BR, BKN, BB, BQ, BKI, BB, BKN, BR],
+    [BP, BP, BP, BP, BP, BP, BP, BP],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [WP, WP, WP, WP, WP, WP, WP, WP],
+    [WR, WKN, WB, WQ, WKI, WB, WKN, WR],
+];
+
+// Checking Enemies
+gameBoard.addEventListener('click', (evt) => {
+    const eventTarget = evt.target;
+    const activeFigure = document.querySelector('.figure_selected');
+    if (playerMove) {
+        if ((eventTarget.classList.contains('figure')) // if target is figure
+            && (board[eventTarget.dataset.y - 1][eventTarget.dataset.x - 1][0] == 'w')) { // if target is ally
+            selectMove(eventTarget);
+        } if ((eventTarget.classList.contains('game-board__cell_active')) // if it is a regular move
+            || ((eventTarget.classList.contains('figure_active')) && (activeFigure))) { // if it is move with eating
+            takeMove(eventTarget);
+            playerMove = false;
+        }
+    } else {
+        if ((eventTarget.classList.contains('figure')) // if target is figure
+            && (board[eventTarget.dataset.y - 1][eventTarget.dataset.x - 1][0] == 'b')) { // if target is foe
+            selectMove(eventTarget);
+        } if ((eventTarget.classList.contains('game-board__cell_active')) // if it is a regular move
+            || ((eventTarget.classList.contains('figure_active')) && (activeFigure))) { // if it is move with eating
+            takeMove(eventTarget);
+            playerMove = true;
+        }
+    }
+})
+
+//  Start
+const start = () => {
+    for (let row in board) {
+        for (let column in board[row]) {
+            const newCell = document.createElement('div');
+            newCell.classList.add('game-board__cell');
+            newCell.setAttribute('data-x', Number(column) + 1);
+            newCell.setAttribute('data-y', Number(row) + 1);
+            if (row % 2 == 0) {
+                if (column % 2 == 0) {
+                    newCell.classList.add('black');
+                }
+            } else {
+                if (column % 2 !== 0) {
+                    newCell.classList.add('black');
+                }
+            };
+            gameBoard.appendChild(newCell);
+            drawFigure(board[row][column], column, row);
+        }
+    }
+}
+const drawFigure = (figure, pos_x, pos_y) => {
+    if (figure !== null) {
+        const newFigure = document.createElement('img');
+        newFigure.setAttribute('data-x', Number(pos_x) + 1);
+        newFigure.setAttribute('data-y', Number(pos_y) + 1);
+        newFigure.style.left = `${10 + 40 * pos_x}px`;
+        newFigure.style.top = `${10 + 40 * pos_y}px`;
+
+        switch (figure) {
+            // Draw Pawns 
+            case WP:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('pawn');
+                newFigure.src = `${src}/w_pawn.png`;
+                break;
+            case BP:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('pawn');
+                newFigure.src = `${src}/b_pawn.png`;
+                break;
+
+            // Draw Rooks
+            case WR:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('rook');
+                newFigure.src = `${src}/w_rook.webp`;
+                break;
+            case BR:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('rook');
+                newFigure.src = `${src}/b_rook.jpg`;
+                break;
+
+            // Draw Knites
+            case WKN:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('knite');
+                newFigure.src = `${src}/w_knite.png`;
+                break;
+            case BKN:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('knite');
+                newFigure.src = `${src}/b_knite.png`;
+                break;
+
+            // Draw Bishops
+            case WB:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('bishop');
+                newFigure.src = `${src}/w_bishop.webp`;
+                break;
+            case BB:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('bishop');
+                newFigure.src = `${src}/b_bishop.webp`;
+                break;
+
+            // Draw Queens
+            case WQ:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('queen');
+                newFigure.src = `${src}/w_queen.jpeg`;
+                break;
+            case BQ:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('queen');
+                newFigure.src = `${src}/b_queen.jpg`;
+                break;
+
+            // Draw Kings
+            case WKI:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('king');
+                newFigure.src = `${src}/w_king.png`;
+                break;
+            case BKI:
+                newFigure.classList.add('figure');
+                newFigure.classList.add('king');
+                newFigure.src = `${src}/b_king.webp`;
+                break;
+        }
+        gameBoard.appendChild(newFigure);
+    }
+}
+
+// Selecting
+const selectMove = (element) => {
+    // Check other elements
+    const selectedElement = document.querySelector('.figure_selected');
+    if (selectedElement == element) {
+        element.classList.remove('figure_selected');
+        nullActive();
+    } else {
+        if (selectedElement !== null) {
+            selectedElement.classList.remove('figure_selected');
+        }
+        nullActive();
+
+        const elementPosX = element.dataset.x;
+        const elementPosY = element.dataset.y;
+        const elementName = board[elementPosY - 1][elementPosX - 1];
+
+        // Hover selected element
+        element.classList.add('figure_selected');
+
+        // Hover cells for move
+        switch (elementName[1]) {
+            case 'p':
+                pawnCheck(elementName[0], +elementPosX, +elementPosY);
+                break;
+            case 'r':
+                rookCheck(elementName[0], +elementPosX, +elementPosY);
+                break;
+            case 'b':
+                bishopCheck(elementName[0], +elementPosX, +elementPosY);
+                break;
+            case 'q':
+                rookCheck(elementName[0], +elementPosX, +elementPosY);
+                bishopCheck(elementName[0], +elementPosX, +elementPosY);
+                break;
+            case 'k':
+                if (elementName[2] == 'n') {
+                    console.log('Конь');
+                } else {
+                    kingCheck(elementName[0], +elementPosX, +elementPosY)
+                }
+                break;
+        }
+    }
+}
+
+const nullActive = () => {
+    const activeCells = document.querySelectorAll('.game-board__cell_active');
+    const activeFigure = document.querySelector('.figure_active');
+    if (activeFigure) {
+        activeFigure.classList.remove('figure_active');
+    }
+    for (let c = 0; c < activeCells.length; c++) {
+        activeCells[c].classList.remove('game-board__cell_active');
+    }
+}
+
+// Moving 
+const takeMove = (selectedCellPos) => {
+    const selectedFigure = document.querySelector('.figure_selected');
+
+    // find figure's coords
+    const selectedFigurePosX = selectedFigure.dataset.x;
+    const selectedFigurePosY = selectedFigure.dataset.y;
+
+    // find cell's coords
+    const selectedCellPosX = selectedCellPos.dataset.x;
+    const selectedCellPosY = selectedCellPos.dataset.y;
+
+    // Eating other figure
+    if (board[selectedCellPosY - 1][selectedCellPosX - 1] !== null) {
+        const eatenEnemies = document.querySelectorAll(`[data-x='${selectedCellPosX}']`);
+        for (let e = 0; e < eatenEnemies.length; e++) {
+            if ((eatenEnemies[e].dataset.y == selectedCellPosY)
+                && eatenEnemies[e].classList.contains('figure')) {
+                eatenEnemies[e].style.display = 'none';
+            }
+        }
+    }
+
+    //  Changing board possitions
+    const selectedFigureName = board[selectedFigurePosY - 1][selectedFigurePosX - 1];
+    board[selectedFigurePosY - 1][selectedFigurePosX - 1] = null;
+    board[selectedCellPosY - 1][selectedCellPosX - 1] = selectedFigureName;
+
+
+    // Changing figure possition
+    selectedFigure.setAttribute('data-x', Number(selectedCellPosX));
+    selectedFigure.setAttribute('data-y', Number(selectedCellPosY));
+    selectedFigure.style.left = `${10 + 40 * (selectedCellPosX - 1)}px`;
+    selectedFigure.style.top = `${10 + 40 * (selectedCellPosY - 1)}px`;
+
+    // Null
+    selectedFigure.classList.remove('figure_selected');
+    nullActive();
+
+    // Check if pawn riched the edge of the board
+    if (((selectedFigure.dataset.y == 1) || (selectedFigure.dataset.y == 8)) && selectedFigure.classList.contains('pawn')) {
+        selectedFigure.classList.remove('pawn')
+        selectedFigure.classList.add('queen')
+        if (playerMove) {
+            selectedFigure.src = `${src}/w_queen.jpeg`
+        } else {
+            selectedFigure.src = `${src}/b_queen.jpg`
+        }
+    }
+    gameBoard.classList.toggle('game-board_active');
+}
+
+start();
---
+++ js/rook.js
@@ -0,0 +1,96 @@
+import { board } from "./main.js";
+
+export const rookCheck = (color, pos_x, pos_y) => {
+
+    const horizontalCells = document.querySelectorAll(`[data-y='${pos_y}'].game-board__cell`);
+    const verticalCells = document.querySelectorAll(`[data-x='${pos_x}'].game-board__cell`);
+
+    // Right direction
+    let searchingRight = true;
+    let c = 0;
+    while (searchingRight) {
+        if ((horizontalCells[c].dataset.x > pos_x)) {
+            horizontalCells[c].classList.add('game-board__cell_active');
+        } if ((horizontalCells[c].dataset.x > pos_x)
+            && (board[pos_y - 1][c] !== null)
+            && (board[pos_y - 1][c][0] == color)) {
+            horizontalCells[c].classList.remove('game-board__cell_active');
+            searchingRight = false;
+        } if ((horizontalCells[c].dataset.x > pos_x)
+            && (board[pos_y - 1][c] !== null)
+            && (board[pos_y - 1][c][0] !== color)) {
+            let activeFigure = document.querySelector(`[data-x='${c + 1}'][data-y='${pos_y}'].figure`);
+            activeFigure.classList.add('figure_active');
+            searchingRight = false;
+        } if (c == 7) {
+            searchingRight = false;
+        }
+        c++;
+    }
+    // Left direction
+    let searchingLeft = true;
+    c = 7;
+    while (searchingLeft) {
+        if ((horizontalCells[c].dataset.x < pos_x)) {
+            horizontalCells[c].classList.add('game-board__cell_active');
+        } if ((horizontalCells[c].dataset.x < pos_x)
+            && (board[pos_y - 1][c] !== null)
+            && (board[pos_y - 1][c][0] == color)) {
+            horizontalCells[c].classList.remove('game-board__cell_active');
+            searchingLeft = false;
+        } if ((horizontalCells[c].dataset.x < pos_x)
+            && (board[pos_y - 1][c] !== null)
+            && (board[pos_y - 1][c][0] !== color)) {
+            let activeFigure = document.querySelector(`[data-x='${c + 1}'][data-y='${pos_y}'].figure`);
+            activeFigure.classList.add('figure_active');
+            searchingLeft = false;
+        } if (c == 0) {
+            searchingLeft = false;
+        }
+        c--;
+    }
+    // Down direction
+    let searchingDown = true;
+    c = 0;
+    while (searchingDown) {
+        if ((verticalCells[c].dataset.y > pos_y)) {
+            verticalCells[c].classList.add('game-board__cell_active');
+        } if ((verticalCells[c].dataset.y > pos_y)
+            && (board[c][pos_x - 1] !== null)
+            && (board[c][pos_x - 1][0] == color)) {
+            verticalCells[c].classList.remove('game-board__cell_active');
+            searchingDown = false;
+        } if ((verticalCells[c].dataset.y > pos_y)
+            && (board[c][pos_x - 1] !== null)
+            && (board[c][pos_x - 1][0] !== color)) {
+            let activeFigure = document.querySelector(`[data-x='${pos_x}'][data-y='${c + 1}'].figure`);
+            activeFigure.classList.add('figure_active');
+            searchingDown = false;
+        } if (c == 7) {
+            searchingDown = false;
+        }
+        c++;
+    }
+    // Up direction
+    let searchingUp = true;
+    c = 7;
+    while (searchingUp) {
+        if ((verticalCells[c].dataset.y < pos_y)) {
+            verticalCells[c].classList.add('game-board__cell_active');
+        } if ((verticalCells[c].dataset.y < pos_y)
+            && (board[c][pos_x - 1] !== null)
+            && (board[c][pos_x - 1][0] == color)) {
+            verticalCells[c].classList.remove('game-board__cell_active');
+            searchingUp = false;
+        } if ((verticalCells[c].dataset.y < pos_y)
+            && (board[c][pos_x - 1] !== null)
+            && (board[c][pos_x - 1][0] !== color)) {
+            let activeFigure = document.querySelector(`[data-x='${pos_x}'][data-y='${c + 1}'].figure`);
+            activeFigure.classList.add('figure_active');
+            searchingUp = false;
+        } if (c == 0) {
+            searchingUp = false;
+        }
+        c--;
+    }
+}
